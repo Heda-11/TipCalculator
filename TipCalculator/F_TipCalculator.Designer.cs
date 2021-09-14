ca7313d40d170e17eb18e914ccdd31bfe3267c6c
Heda-11 <pandeyhrithik71@gmail.com>
2021-09-14 16:57:11 +0530

    Add project files.

---
+++ TipCalculator/F_TipCalculator.Designer.cs
@@ -0,0 +1,212 @@
+﻿
+namespace TipCalculator
+{
+    partial class TipCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TipCalculator));
+            this.Txt_BillAmount = new System.Windows.Forms.TextBox();
+            this.Txt_TipPercentage = new System.Windows.Forms.TextBox();
+            this.NUD_Diners = new System.Windows.Forms.NumericUpDown();
+            this.DarkMode_BTN = new System.Windows.Forms.Button();
+            this.Calculate_BTN = new System.Windows.Forms.Button();
+            this.lbl_BillAmount = new System.Windows.Forms.Label();
+            this.lbl_TipPercent = new System.Windows.Forms.Label();
+            this.lbl_Diners = new System.Windows.Forms.Label();
+            this.lbl_TTip = new System.Windows.Forms.Label();
+            this.lbl_Total = new System.Windows.Forms.Label();
+            this.CB_Languages = new System.Windows.Forms.ComboBox();
+            this.lbl_Heading = new System.Windows.Forms.Label();
+            this.lbl_Lang = new System.Windows.Forms.Label();
+            this.lbl_TotalPerPerson = new System.Windows.Forms.Label();
+            this.lbl_TipPerPerson = new System.Windows.Forms.Label();
+            this.lbl_CDate = new System.Windows.Forms.Label();
+            this.lbl_Date = new System.Windows.Forms.Label();
+            this.Clear_BTN = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.NUD_Diners)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Txt_BillAmount
+            // 
+            resources.ApplyResources(this.Txt_BillAmount, "Txt_BillAmount");
+            this.Txt_BillAmount.Name = "Txt_BillAmount";
+            // 
+            // Txt_TipPercentage
+            // 
+            resources.ApplyResources(this.Txt_TipPercentage, "Txt_TipPercentage");
+            this.Txt_TipPercentage.Name = "Txt_TipPercentage";
+            // 
+            // NUD_Diners
+            // 
+            resources.ApplyResources(this.NUD_Diners, "NUD_Diners");
+            this.NUD_Diners.Name = "NUD_Diners";
+            // 
+            // DarkMode_BTN
+            // 
+            resources.ApplyResources(this.DarkMode_BTN, "DarkMode_BTN");
+            this.DarkMode_BTN.Name = "DarkMode_BTN";
+            this.DarkMode_BTN.UseVisualStyleBackColor = true;
+            this.DarkMode_BTN.Click += new System.EventHandler(this.DarkMode_BTN_Click);
+            // 
+            // Calculate_BTN
+            // 
+            resources.ApplyResources(this.Calculate_BTN, "Calculate_BTN");
+            this.Calculate_BTN.Name = "Calculate_BTN";
+            this.Calculate_BTN.UseVisualStyleBackColor = true;
+            this.Calculate_BTN.Click += new System.EventHandler(this.Calculate_BTN_Click);
+            // 
+            // lbl_BillAmount
+            // 
+            resources.ApplyResources(this.lbl_BillAmount, "lbl_BillAmount");
+            this.lbl_BillAmount.Name = "lbl_BillAmount";
+            // 
+            // lbl_TipPercent
+            // 
+            resources.ApplyResources(this.lbl_TipPercent, "lbl_TipPercent");
+            this.lbl_TipPercent.Name = "lbl_TipPercent";
+            // 
+            // lbl_Diners
+            // 
+            resources.ApplyResources(this.lbl_Diners, "lbl_Diners");
+            this.lbl_Diners.Name = "lbl_Diners";
+            // 
+            // lbl_TTip
+            // 
+            resources.ApplyResources(this.lbl_TTip, "lbl_TTip");
+            this.lbl_TTip.Name = "lbl_TTip";
+            // 
+            // lbl_Total
+            // 
+            resources.ApplyResources(this.lbl_Total, "lbl_Total");
+            this.lbl_Total.Name = "lbl_Total";
+            // 
+            // CB_Languages
+            // 
+            resources.ApplyResources(this.CB_Languages, "CB_Languages");
+            this.CB_Languages.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.CB_Languages.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.CB_Languages.FormattingEnabled = true;
+            this.CB_Languages.Items.AddRange(new object[] {
+            resources.GetString("CB_Languages.Items"),
+            resources.GetString("CB_Languages.Items1"),
+            resources.GetString("CB_Languages.Items2"),
+            resources.GetString("CB_Languages.Items3")});
+            this.CB_Languages.Name = "CB_Languages";
+            this.CB_Languages.SelectedIndexChanged += new System.EventHandler(this.Languages_CB_SelectedIndexChanged);
+            // 
+            // lbl_Heading
+            // 
+            resources.ApplyResources(this.lbl_Heading, "lbl_Heading");
+            this.lbl_Heading.Name = "lbl_Heading";
+            // 
+            // lbl_Lang
+            // 
+            resources.ApplyResources(this.lbl_Lang, "lbl_Lang");
+            this.lbl_Lang.Name = "lbl_Lang";
+            // 
+            // lbl_TotalPerPerson
+            // 
+            resources.ApplyResources(this.lbl_TotalPerPerson, "lbl_TotalPerPerson");
+            this.lbl_TotalPerPerson.Name = "lbl_TotalPerPerson";
+            // 
+            // lbl_TipPerPerson
+            // 
+            resources.ApplyResources(this.lbl_TipPerPerson, "lbl_TipPerPerson");
+            this.lbl_TipPerPerson.Name = "lbl_TipPerPerson";
+            // 
+            // lbl_CDate
+            // 
+            resources.ApplyResources(this.lbl_CDate, "lbl_CDate");
+            this.lbl_CDate.Name = "lbl_CDate";
+            // 
+            // lbl_Date
+            // 
+            resources.ApplyResources(this.lbl_Date, "lbl_Date");
+            this.lbl_Date.Name = "lbl_Date";
+            // 
+            // Clear_BTN
+            // 
+            resources.ApplyResources(this.Clear_BTN, "Clear_BTN");
+            this.Clear_BTN.Name = "Clear_BTN";
+            this.Clear_BTN.UseVisualStyleBackColor = true;
+            this.Clear_BTN.Click += new System.EventHandler(this.Clear_BTN_Click);
+            // 
+            // TipCalculator
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.Controls.Add(this.Clear_BTN);
+            this.Controls.Add(this.lbl_Date);
+            this.Controls.Add(this.lbl_CDate);
+            this.Controls.Add(this.lbl_Lang);
+            this.Controls.Add(this.lbl_Heading);
+            this.Controls.Add(this.CB_Languages);
+            this.Controls.Add(this.lbl_Total);
+            this.Controls.Add(this.lbl_TTip);
+            this.Controls.Add(this.lbl_Diners);
+            this.Controls.Add(this.lbl_TipPercent);
+            this.Controls.Add(this.lbl_BillAmount);
+            this.Controls.Add(this.Calculate_BTN);
+            this.Controls.Add(this.DarkMode_BTN);
+            this.Controls.Add(this.lbl_TotalPerPerson);
+            this.Controls.Add(this.lbl_TipPerPerson);
+            this.Controls.Add(this.NUD_Diners);
+            this.Controls.Add(this.Txt_TipPercentage);
+            this.Controls.Add(this.Txt_BillAmount);
+            this.Name = "TipCalculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.NUD_Diners)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Txt_BillAmount;
+        private System.Windows.Forms.TextBox Txt_TipPercentage;
+        private System.Windows.Forms.NumericUpDown NUD_Diners;
+        private System.Windows.Forms.Button DarkMode_BTN;
+        private System.Windows.Forms.Button Calculate_BTN;
+        private System.Windows.Forms.Label lbl_BillAmount;
+        private System.Windows.Forms.Label lbl_TipPercent;
+        private System.Windows.Forms.Label lbl_Diners;
+        private System.Windows.Forms.Label lbl_TTip;
+        private System.Windows.Forms.Label lbl_Total;
+        private System.Windows.Forms.ComboBox CB_Languages;
+        private System.Windows.Forms.Label lbl_Heading;
+        private System.Windows.Forms.Label lbl_Lang;
+        private System.Windows.Forms.Label lbl_TotalPerPerson;
+        private System.Windows.Forms.Label lbl_TipPerPerson;
+        private System.Windows.Forms.Label lbl_CDate;
+        private System.Windows.Forms.Label lbl_Date;
+        private System.Windows.Forms.Button Clear_BTN;
+    }
+}
+
---
+++ TipCalculator/F_TipCalculator.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+using System.Globalization;
+
+namespace TipCalculator
+{
+    public partial class TipCalculator : Form
+    {
+        public TipCalculator()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           //Setting Default Culture and Date According to it
+            Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-Us");
+            Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("en-Us");
+            var date1 = DateTime.Now;
+            string date = date1.ToShortDateString();
+            lbl_CDate.Text = date;
+
+        }
+
+
+        private void Calculate_BTN_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double Tip_Per_Person = 0;
+                double Total_Per_Person = 0;
+                double Bill = Convert.ToDouble(Txt_BillAmount.Text);
+                double Tip_Percentage = Convert.ToDouble(this.Txt_TipPercentage.Text);
+                int Total_People = Convert.ToInt32(NUD_Diners.Value);
+                if (Total_People >= 1)
+                {
+                    //Calculating Tip and Total Per Person 
+                    if (Bill >= 0 && Tip_Percentage >= 0)
+                    {
+                        Tip_Per_Person = Math.Round(GetTipPerPerson(Bill, Tip_Percentage, Total_People), 2);
+                        Total_Per_Person = Math.Round((Bill / Total_People) + Tip_Per_Person, 2);
+                        //Converting Double to currency format
+                        lbl_TipPerPerson.Text = Tip_Per_Person.ToString("C");
+                        lbl_TotalPerPerson.Text = Total_Per_Person.ToString("C");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Bill Amount or Tip Percentage Cant be Less Than Zero(0)");
+
+                    }
+                    
+                }
+                else
+                {
+                    MessageBox.Show("Sorry The Number Of Diners cant be less than 1");
+                }
+            }
+            catch (FormatException )
+            {
+                MessageBox.Show("Please Enter a Valid Amount");
+            }
+            catch (Exception z)
+            {
+
+                MessageBox.Show(z + "/n Sorry ,Something Went Wrong");
+            }
+        }
+        Double GetTipPerPerson(Double Bill, Double Tip_Percentage, Double People)
+        {
+            double TipPerPerson = ((Bill * Tip_Percentage) / 100) / People;
+            return TipPerPerson;
+        }
+        public bool isActive=false;
+        private void DarkMode_BTN_Click(object sender, EventArgs e)
+        {
+            //Checking Current Mode
+            if (isActive == false)
+            {
+                isActive = DarkMode_ON();
+            }
+            else 
+                isActive=DarkMode_OFF();
+        }
+        bool DarkMode_ON() {
+            //Design for the 'DarkMode'
+            this.BackColor = Color.FromArgb(30,30,30);
+            Calculate_BTN.BackColor = Color.FromArgb(18, 18, 18);
+            Calculate_BTN.ForeColor = Color.FromArgb(245, 245, 245);
+            Clear_BTN.BackColor = Color.FromArgb(18, 18, 18);
+            Clear_BTN.ForeColor = Color.FromArgb(245, 245, 245);
+            Txt_BillAmount.BackColor = Color.FromArgb(18, 18, 18);
+            Txt_BillAmount.ForeColor = Color.FromArgb(245, 245, 245);
+            Txt_TipPercentage.BackColor = Color.FromArgb(18, 18, 18);
+            Txt_TipPercentage.ForeColor = Color.FromArgb(245, 245, 245);
+            NUD_Diners.BackColor = Color.FromArgb(18, 18, 18);
+            NUD_Diners.ForeColor = Color.FromArgb(245, 245, 245);
+            lbl_TipPerPerson.ForeColor = Color.FromArgb(245, 245, 245);
+            lbl_TotalPerPerson.ForeColor = Color.FromArgb(245, 245, 245);
+            lbl_BillAmount.ForeColor = Color.FromArgb(245, 245, 245);
+            lbl_TipPercent.ForeColor = Color.FromArgb(245, 245, 245);
+            lbl_Diners.ForeColor = Color.FromArgb(245, 245, 245);
+            lbl_Total.ForeColor = Color.FromArgb(245, 245, 245);
+            lbl_TTip.ForeColor = Color.FromArgb(245, 245, 245);
+            lbl_Lang.ForeColor = Color.FromArgb(245, 245, 245);
+            lbl_Heading.ForeColor = Color.FromArgb(245, 245, 245);
+            lbl_CDate.ForeColor = Color.FromArgb(245, 245, 245);
+            lbl_Date.ForeColor = Color.FromArgb(245, 245, 245);
+            DarkMode_BTN.Image = Properties.Resources.DarkMode;
+            DarkMode_BTN.Text = "";
+            return true;
+
+        }
+        bool DarkMode_OFF() {
+            //Design for the 'LightMode'
+            this.BackColor = Color.FromArgb(230, 230, 230);
+            Calculate_BTN.BackColor = Color.FromArgb(245, 245, 245);
+            Calculate_BTN.ForeColor = Color.FromArgb(18, 18, 18);
+            Clear_BTN.BackColor = Color.FromArgb(245, 245, 245);
+            Clear_BTN.ForeColor = Color.FromArgb(18, 18, 18);
+            Txt_BillAmount.BackColor = Color.FromArgb(245, 245, 245);
+            Txt_BillAmount.ForeColor = Color.FromArgb(18, 18, 18);
+            Txt_TipPercentage.BackColor = Color.FromArgb(245, 245, 245);
+            Txt_TipPercentage.ForeColor = Color.FromArgb(18, 18, 18);
+            NUD_Diners.BackColor = Color.FromArgb(245, 245, 245);
+            NUD_Diners.ForeColor = Color.FromArgb(18, 18, 18);
+            lbl_TipPerPerson.ForeColor = Color.FromArgb(245, 245, 245);
+            lbl_TipPerPerson.ForeColor = Color.FromArgb(18, 18, 18);
+            lbl_TotalPerPerson.ForeColor = Color.FromArgb(18, 18, 18);
+            lbl_BillAmount.ForeColor = Color.FromArgb(18, 18, 18);
+            lbl_TipPercent.ForeColor = Color.FromArgb(18, 18, 18);
+            lbl_Diners.ForeColor = Color.FromArgb(18, 18, 18);
+            lbl_Total.ForeColor = Color.FromArgb(18, 18, 18);
+            lbl_TTip.ForeColor = Color.FromArgb(18, 18, 18);
+            lbl_Lang.ForeColor = Color.FromArgb(18, 18, 18);
+            lbl_Heading.ForeColor = Color.FromArgb(18, 18, 18);
+            lbl_CDate.ForeColor = Color.FromArgb(18, 18, 18);
+            lbl_Date.ForeColor = Color.FromArgb(18, 18, 18);
+            DarkMode_BTN.Image = Properties.Resources.LightMode;
+            return false;
+        }
+
+        private void Languages_CB_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Changing Language and Culture according to user Input
+            try
+            {
+                switch (CB_Languages.SelectedIndex)
+                {
+                    case 0:
+                        CultureInfo info = new CultureInfo("en-Us");
+                        Thread.CurrentThread.CurrentCulture = info;
+                        Thread.CurrentThread.CurrentUICulture = info;
+                        break;
+                    case 1:
+                        info = new CultureInfo("hi-In");
+                        Thread.CurrentThread.CurrentCulture = info;
+                        Thread.CurrentThread.CurrentUICulture = info;
+                        break;
+                    case 2:
+                        info = new CultureInfo("fr-FR");
+                        Thread.CurrentThread.CurrentCulture = info;
+                        Thread.CurrentThread.CurrentUICulture = info;
+                        break;
+                    case 3:
+                        info = new CultureInfo("ru");
+                        Thread.CurrentThread.CurrentCulture = info;
+                        Thread.CurrentThread.CurrentUICulture = info;
+                        break;
+
+                }
+                this.Controls.Clear();
+                InitializeComponent();
+                var date1 = DateTime.Now;
+                string date = date1.ToShortDateString();
+                lbl_CDate.Text = date;
+            }
+            catch (Exception )
+            {
+                MessageBox.Show("Sorry, Something Went Wrong,Please Contact Us.");
+            }
+            
+           }
+
+        private void Clear_BTN_Click(object sender, EventArgs e)
+        {
+            //Clearing all the Input and Output Fields
+            Txt_BillAmount.Clear();
+            Txt_TipPercentage.Clear();
+            NUD_Diners.Value = 0;
+            lbl_TotalPerPerson.Text = "";
+            lbl_TipPerPerson.Text = "";
+        }
+    }
+}
